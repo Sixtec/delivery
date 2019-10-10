01231320920a895574aa20ef9b0898e3cdbe332e
Mário Lucas <maryosk8bkt@gmail.com>
2019-10-10 16:17:16 -0300

    add helpers load and alert

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'default');
+$autoload['helper'] = array('url', 'file', 'default', 'alert', 'template');
 
 /*
 | -------------------------------------------------------------------
